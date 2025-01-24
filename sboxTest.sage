5c0d6ae8ddbaa08979496f4454e52aca8bc95296
HalilKaplan <4.halilkaplan@gmail.com>
2025-01-24 16:36:07 +0300

    .

--- sboxTest.sage
+++ sboxTest.sage
@@ -9,13 +9,12 @@ SBOX_DEC = [int(x) for x in sbox_hex]
 # Create the S-box in SageMath
 S = SBox(SBOX_DEC)
 
-print("Max value in LAT:",S.linear_branch_number())
-
-print("Fixed points of S:",S.fixed_points())
 
 print("S is linear: ",S.has_linear_structure())
 
 print("S is balanced: ",S.is_balanced())
+
+print("Fixed points of S:",S.fixed_points())
 
 print("S is APN: ",S.is_almost_bent())
 
@@ -28,23 +27,24 @@ print("S is plateaued: ",S.is_plateaued())
 print("Maximal algebraic degree of all its component functions",S.max_degree() )
 
 print("Minimal algebraic degree of all its component functions",S.min_degree() )
+print("Max value in DDT:",S.maximal_difference_probability_absolute())
+print("Max value in DDT:",S.maximal_difference_probability_absolute())
+
+print("Max value in LAT:",S.linear_branch_number())
+
+print("Max value in BCT:",S.boomerang_uniformity())
 
 print("##### Autocorrelation table #####")
 
 #print(S.autocorrelation_table())
 
-
 print("##### Boomerang connectivity table #####")
 
 #print(S.boomerang_connectivity_table())
 
-print("Max value in BCT:",S.boomerang_uniformity())
-
 print("##### DDT #####")
 
-#print(S.difference_distribution_table())
-
-print("Max value in DDT:",S.maximal_difference_probability_absolute())
+print(S.difference_distribution_table())
 
 print("Branch number of S:",S.differential_branch_number())
 
--- sboxTest.sage.py
+++ sboxTest.sage.py
@@ -14,36 +14,12 @@ SBOX_DEC = [int(x) for x in sbox_hex]
 # Create the S-box in SageMath
 S = SBox(SBOX_DEC)
 
-print("##### Autocorrelation table #####")
-
-#print(S.autocorrelation_table())
-
-
-print("##### Boomerang connectivity table #####")
-
-#print(S.boomerang_connectivity_table())
-
-print("Max value in BCT:",S.boomerang_uniformity())
-
-print("##### DDT #####")
-
-#print(S.difference_distribution_table())
-
-print("Max value in DDT:",S.maximal_difference_probability_absolute())
-
-print("Branch number of S:",S.differential_branch_number())
-
-print("##### LAT #####")
-
-#print(S.linear_approximation_table())
-
-print("Max value in LAT:",S.linear_branch_number())
-
-print("Fixed points of S:",S.fixed_points())
 
 print("S is linear: ",S.has_linear_structure())
 
 print("S is balanced: ",S.is_balanced())
+
+print("Fixed points of S:",S.fixed_points())
 
 print("S is APN: ",S.is_almost_bent())
 
@@ -56,6 +32,32 @@ print("S is plateaued: ",S.is_plateaued())
 print("Maximal algebraic degree of all its component functions",S.max_degree() )
 
 print("Minimal algebraic degree of all its component functions",S.min_degree() )
+print("Max value in DDT:",S.maximal_difference_probability_absolute())
+print("Max value in DDT:",S.maximal_difference_probability_absolute())
+
+print("Max value in LAT:",S.linear_branch_number())
+
+print("Max value in BCT:",S.boomerang_uniformity())
+
+print("##### Autocorrelation table #####")
+
+#print(S.autocorrelation_table())
+
+print("##### Boomerang connectivity table #####")
+
+#print(S.boomerang_connectivity_table())
+
+print("##### DDT #####")
+
+print(S.difference_distribution_table())
+
+print("Branch number of S:",S.differential_branch_number())
+
+print("##### LAT #####")
+
+#print(S.linear_approximation_table())
+
+
 
 #print("List of polynomials satisfying this S-box",S.polynomials())
 
