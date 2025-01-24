4bfd190ddefe45a76813ec6c9326eb5c61da8a7f
HalilKaplan <4.halilkaplan@gmail.com>
2025-01-24 16:18:05 +0300

    .

--- sboxTest.sage.py
+++ sboxTest.sage.py
@@ -25,3 +25,37 @@ print("##### Boomerang connectivity table #####")
 
 print("Max value in BCT:",S.boomerang_uniformity())
 
+print("##### DDT #####")
+
+#print(S.difference_distribution_table())
+
+print("Max value in DDT:",S.maximal_difference_probability_absolute())
+
+print("Branch number of S:",S.differential_branch_number())
+
+print("##### LAT #####")
+
+#print(S.linear_approximation_table())
+
+print("Max value in LAT:",S.linear_branch_number())
+
+print("Fixed points of S:",S.fixed_points())
+
+print("S is linear: ",S.has_linear_structure())
+
+print("S is balanced: ",S.is_balanced())
+
+print("S is APN: ",S.is_almost_bent())
+
+print("S is bent: ",S.is_bent())
+
+print("S is has an inverse: ",S.is_involution())
+
+print("S is plateaued: ",S.is_plateaued())
+
+print("Maximal algebraic degree of all its component functions",S.max_degree() )
+
+print("Minimal algebraic degree of all its component functions",S.min_degree() )
+
+#print("List of polynomials satisfying this S-box",S.polynomials())
+
